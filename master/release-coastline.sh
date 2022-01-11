142844a930721b20f182658fe82db17456b5a125
wohenbushuang <wohenbushuang@users.noreply.github.com>
2022-01-12 01:58:24 +0800

    Create release-coastline.sh
    [skip ci]

---
+++ master/release-coastline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#  release-coastline.sh
+#
+
+set -euo pipefail
+
+LOCK_FILE=~/log/running
+
+if [ -f $LOCK_FILE ]; then
+    echo "Update process is running. Can not release coastline."
+    exit 1
+fi
+
+date >>~/log/release-coastline.log
+
+cd /data/compare
+
+NEWEST=$(ls mask-20* | tail -1)
+
+rm -f mask-good.tiff
+ln -s "$NEWEST" mask-good.tiff
+
+mv /data/new/* /data/good/
+cp /data/good/* /data/new/
