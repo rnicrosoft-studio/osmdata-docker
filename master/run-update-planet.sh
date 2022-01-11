9ce074c5d08951eb058096b859c398df7d0d7c63
wohenbushuang <wohenbushuang@users.noreply.github.com>
2022-01-11 20:08:52 +0800

    Create run-update-planet.sh

---
+++ master/run-update-planet.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+#  run-update-planet.sh
+#
+
+set -euo pipefail
+
+if [ "$USER" != "robot" ]; then
+    echo "Must be run as user robot"
+    exit 1
+fi
+
+# SERVER=update-planet
+
+# cx41: 4 CPUs, 16 GB RAM, 160 GB disk
+# STYPE=cx41
+
+# VOLID=$(hcloud volume describe -o json planet | jq .id)
+
+# printf "#cloud-config\nmounts:\n    - [ 'ID=scsi-0HC_Volume_${VOLID}', '/mnt' ]\n" | \
+# hcloud server create \
+#     --name $SERVER \
+#     --location nbg1 \
+#     --type $STYPE \
+#     --image debian-11 \
+#     --ssh-key admin \
+#     --user-data-from-file ~/osmdata/servers/$SERVER.yml \
+#     --user-data-from-file ~/users.yml \
+#     --user-data-from-file ~/ssh/keys.yml \
+#     --user-data-from-file - \
+#     --volume planet
+
+# IP=$(hcloud server ip $SERVER)
+
+# echo "$IP"
+
+# sed -e "s/^IP /${IP} /" ~/ssh/known_hosts >~/.ssh/known_hosts
+
+# echo "Waiting for system to become ready..."
+# sleep 60
+# ssh -o ConnectTimeout=600 "robot@${IP}" cloud-init status --wait
+echo "System initialized."
+
+# ssh "robot@${IP}" mkdir planet
+mkdir planet
+# scp ~/osmdata/scripts/planet/* "robot@${IP}:planet/"
+cp ~/osmdata/scripts/planet/* planet/
+
+# ssh "robot@${IP}" planet/update.sh
+exec planet/update.sh
+# ssh "robot@${IP}" sudo umount /mnt
+
+# hcloud volume detach planet
+
+# hcloud server delete $SERVER
+rm -r planet
+
+echo "run-update-planet done."
